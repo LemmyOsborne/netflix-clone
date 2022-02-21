dfbe8d2c2410029f98304433ec944eae31bc46c2
lemmyosborne <bozhenko.rodion@mail.ru>
2022-02-21 17:04:35 +0300

    test: test for call-to-action form

---
+++ src/__tests__/components/CtaForm.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { CtaForm } from "../../components";
+import { render } from "@testing-library/react";
+
+describe("<CtaFotm/>", () => {
+    it("render <CtaForm/>", () => {
+        const { container, getByText, getByPlaceholderText, getByTestId } = render(
+            <CtaForm direction="row">
+                <CtaForm.InputGroup data-testId="input-group">
+                    <CtaForm.Text>Ready to watch? Enter your email to create or restart your membership.</CtaForm.Text>
+                    <CtaForm.Input placeholder="Email address" />
+                    <CtaForm.Button>Try it now</CtaForm.Button>
+                </CtaForm.InputGroup>
+            </CtaForm>
+        )
+
+        expect(getByText("Ready to watch? Enter your email to create or restart your membership.")).toBeTruthy()
+        expect(getByText("Try it now")).toBeTruthy()
+        expect(getByPlaceholderText("Email address")).toBeTruthy()
+        expect(getByTestId("input-group")).toBeTruthy()
+        expect(container.firstChild).toMatchSnapshot()
+    })
+})
