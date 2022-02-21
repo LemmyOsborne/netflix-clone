c6c0ce403f21f35ce62c7cc757301feea0d0e163
lemmyosborne <bozhenko.rodion@mail.ru>
2022-02-21 14:35:12 +0300

    test: test for footer

---
+++ src/__tests__/components/Footer.test.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Footer } from "../../components";
+import { render } from "@testing-library/react";
+
+describe("<Footer/>", () => {
+    it("render <Footer/> populated with data", () => {
+        const { getByText, container } = render(
+            <Footer>
+            <Footer.Title>Questions? Call 8-800-100-9668</Footer.Title>
+            <Footer.Row>
+                <Footer.Column>
+                    <Footer.Link href="#">FAQ</Footer.Link>
+                    <Footer.Link href="#">Investor Relations</Footer.Link>
+                    <Footer.Link href="#">Privacy</Footer.Link>
+                    <Footer.Link href="#">Speed Test</Footer.Link>
+                </Footer.Column>
+                <Footer.Column>
+                    <Footer.Link href="#">Help Center</Footer.Link>
+                    <Footer.Link href="#">Jobs</Footer.Link>
+                    <Footer.Link href="#">Cookie Preferences</Footer.Link>
+                    <Footer.Link href="#">Legal Notices</Footer.Link>
+                </Footer.Column>
+                <Footer.Column>
+                    <Footer.Link href="#">Account</Footer.Link>
+                    <Footer.Link href="#">Ways to Watch</Footer.Link>
+                    <Footer.Link href="#">Corporate Information</Footer.Link>
+                    <Footer.Link href="#">Only on Netflix</Footer.Link>
+                </Footer.Column>
+                <Footer.Column>
+                    <Footer.Link href="#">Media center</Footer.Link>
+                    <Footer.Link href="#">Terms of Use</Footer.Link>
+                    <Footer.Link href="#">Contact Us</Footer.Link>
+                </Footer.Column>
+            </Footer.Row>
+            <Footer.BottomText>Netflix Russia</Footer.BottomText>
+        </Footer>
+        )
+
+        expect(getByText("Questions? Call 8-800-100-9668")).toBeTruthy()
+        expect(getByText("FAQ")).toBeTruthy()
+        expect(getByText("Investor Relations")).toBeTruthy()
+        expect(getByText("Privacy")).toBeTruthy()
+        expect(getByText("Speed Test")).toBeTruthy()
+        expect(container.firstChild).toMatchSnapshot()
+    })
+})
